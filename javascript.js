63ca1324c893f908693bba28c729765d95817b40
Melissa <melissa.layser22@gmail.com>
2022-05-17 20:11:15 -0700

    Add code to playRound for return string options

--- javascript.js
+++ javascript.js
@@ -5,5 +5,16 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    let cS = computerSelection;
+    let pS = playerSelection;
 
+    if (cS === 'Rock' && pS === 'Rock' || cS === 'Paper' && pS === 'Paper' || cS === 'Scissors' && pS === 'Scissors') {
+        alert('It\'s a tie!');
+    } else if (cS === 'Rock' && pS === 'Paper' || cS === 'Paper' && pS === 'Scissors' || cS === 'Scissors' && pS === 'Rock') {
+        alert(`You win! ${pS} beats ${cS}!`);
+    } else if (cS === 'Rock' && pS === 'Scissors' || cS === 'Paper' && pS === 'Rock' || cS === 'Scissors' && pS === 'Paper') {
+        alert(`You lose! ${cS} beats ${pS}!`);
+    } else {
+        alert('Error. Please choose from one of the following options: "Rock", "Paper", or "Scissors"')
+    }
 }
