fc17517e7121ffc207b277e91076cb0dbd009ec0
Melissa <melissa.layser22@gmail.com>
2022-09-02 15:59:54 -0700

    Clean up code & edit endGame for replay

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 const choices = ['Rock', 'Paper', 'Scissors'];
-const computerSelection = computerPlay(); 
+const computerSelection = computerPlay();
 let counter = 0;
 let scores = {playerScore : 0, computerScore : 0};
 
@@ -16,42 +16,52 @@ function playRound(playerSelection, computerSelection) {
         content.textContent = ('It\'s a tie!');
         scores.playerScore++;
         scores.computerScore++;
-        score.textContent = (`Your score: ${scores.playerScore}, Computer\'s score: ${scores.computerScore}`);
-    } else if (cS === 'Rock' && pS === 'Paper' || cS === 'Paper' && pS === 'Scissors' || cS === 'Scissors' && pS === 'Rock') {
+        score.textContent = (`Your score: ${scores.playerScore},
+          Computer\'s score: ${scores.computerScore}`);
+    } else if (cS === 'Rock' && pS === 'Paper' || cS === 'Paper' &&
+       pS === 'Scissors' || cS === 'Scissors' && pS === 'Rock') {
         content.textContent = (`You win! ${pS} beats ${cS}!`);
         scores.playerScore++;
-        score.textContent = (`Your score: ${scores.playerScore}, Computer\'s score: ${scores.computerScore}`);
-    } else if (cS === 'Rock' && pS === 'Scissors' || cS === 'Paper' && pS === 'Rock' || cS === 'Scissors' && pS === 'Paper') {
+        score.textContent = (`Your score: ${scores.playerScore},
+          Computer\'s score: ${scores.computerScore}`);
+    } else if (cS === 'Rock' && pS === 'Scissors' || cS === 'Paper' &&
+       pS === 'Rock' || cS === 'Scissors' && pS === 'Paper') {
         content.textContent = (`You lose! ${cS} beats ${pS}!`);
         scores.computerScore++;
-        score.textContent = (`Your score: ${scores.playerScore}, Computer\'s score: ${scores.computerScore}`);
+        score.textContent = (`Your score: ${scores.playerScore},
+          Computer\'s score: ${scores.computerScore}`);
     } else {
-        content.textContent = ('Error. Please choose from one of the following options: "Rock", "Paper", or "Scissors"')
+        content.textContent = 
+          ('Error! Something went wrong.')
     }
 }
 
 function endGame(scores) {
     if (scores.playerScore > scores.computerScore) {
-        content.textContent = ('END OF GAME: Victory is yours!');
+        content.textContent = 
+          ('END OF GAME: Victory is yours! Refresh browser to play again.');
     } else if (scores.playerScore < scores.computerScore) {
-        content.textContent = ('END OF GAME: Luh-hoo-zuh-her! Better luck next time.');
+        content.textContent = 
+          ('END OF GAME: Luh-hoo-zuh-her! Better luck next time. Refresh browser to play again.');
     } else {
-        content.textContent = ('END OF GAME: A complete tie. How unsatisfying.');
+        content.textContent = 
+          ('END OF GAME: A complete tie. How unsatisfying. Refresh browser to play again.');
     }
 }
 
 function clickLoop() {
-    if(counter < 5) {
+    if (counter < 5) {
         const buttons = document.querySelectorAll('button');
 
         buttons.forEach((button) => {
             button.addEventListener('click', (event) => {
                 playerSelection = event.target.textContent;
                 playRound(playerSelection, computerPlay());
-                if(counter == 4){
+                if (counter == 4) {
                     endGame(scores);
-                    score.textContent = (`Your score: ${scores.playerScore}, Computer\'s score: ${scores.computerScore}`);
-                    for (var i = 0; i < buttons.length; i++){
+                    score.textContent = (`Your score: ${scores.playerScore},
+                      Computer\'s score: ${scores.computerScore}`);
+                    for (var i = 0; i < buttons.length; i++) {
                         buttons[i].disabled = true;
                     }
                 } else {
@@ -73,8 +83,8 @@ score.classList.add('score');
 
 results.textContent = clickLoop();
 content.textContent = "Rock, Paper, or Scissors??";
-score.textContent = `Your score: ${scores.playerScore}, Computer\'s score: ${scores.computerScore}`;
+score.textContent = `Your score: ${scores.playerScore},
+  Computer\'s score: ${scores.computerScore}`;
 
-results.appendChild(content);
 results.appendChild(score);
 results.insertBefore(content, score);
